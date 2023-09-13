4f312446e32ebb104f0946632a2fe73bf2f64aa5
Vibhu@ <94893243+Usee1234@users.noreply.github.com>
2023-09-13 15:09:25 +0000

    Container add Variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,59 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+let isWrite=true
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+isWrite && console.log("Vibhu")
+isWrite?console.log("True"):console.log("not true")
+
+
+//2
+
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
